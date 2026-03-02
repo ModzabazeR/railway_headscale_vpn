fb538577a89fa99fa74d8ecdace12361c2003de7
ModzabazeR <moddaeng.pps@gmail.com>
2026-03-02 13:38:50 +0700

    fix: update http proxy port

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=0.0.0.0:1055 --outbound-http-proxy-listen=0.0.0.0:1055 &
+./tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=0.0.0.0:1055 --outbound-http-proxy-listen=0.0.0.0:1056 &
 until ./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --login-server=${TAILSCALE_LOGIN_SERVER} ${TAILSCALE_ADDITIONAL_ARGS}; do
   sleep 0.1
 done
